841c0f9076c86870a91ae9957ec4ef5d9ed4681b
Sefa Üncü <sefauncu@gmail.com>
2018-07-18 11:55:43 +0300

    source code

--- spring-boot-mongodb-example/src/main/java/com/kodreis/mongodb/Application.java
+++ spring-boot-mongodb-example/src/main/java/com/kodreis/mongodb/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-	
+	    //start project
 		SpringApplication.run(Application.class, args);
 	}
 
